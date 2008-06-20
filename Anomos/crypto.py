32923916a534f336e9be350ea2c7ca4bcdc7bdf4
Rich Jones <miserlou@gmail.com>
2008-06-20 01:46:33 -0400

    AES functionality implemented. AESEncrypt(k, iv, tx) and AESDecrypt(k, iv, tx) methods.

--- Anomos/crypto.py
+++ Anomos/crypto.py
@@ -3,18 +3,22 @@ crypto.py
 
 anomos. very preliminary. rsa should eventually be replaced with real ssl. requires m2crypto.
 
-padding: pkcs1_oaep_padding
+RSA padding: pkcs1_oaep_padding
+AES cipher: aes_128_cfb
 
-for other functions used directly, look at RSA.py in M2Crypto
+for other functions used directly, look at RSA.py and EVP.py in M2Crypto
 
 """
 
 import sys
 import M2Crypto
 import os
-from M2Crypto import Rand, RSA, util
+import cStringIO
+from binascii import hexlify, unhexlify
+from M2Crypto import Rand, RSA, util, EVP
 
 key_size=1024
+secret = "Call me subwoofa cause I push so much base!"
 
 def saveNewPEM():
     Rand.load_file('randpool.dat', -1) 
@@ -45,6 +49,7 @@ def loadKeysFromPEM():
     
     return (pub, pvt)
 
+##32 random bytes
 def getRand32():
     Rand.load_file('randpool.dat', -1)
     rb = Rand.rand_bytes(32);
@@ -57,13 +62,50 @@ def getNewAES():
 def getNewIV():
     return getRand32()
 
+##this is where the actual ciphering is done
+def cipher_filter(cipher, inf, outf):
+        while 1:
+            buf=inf.read()
+            if not buf:
+                break
+            outf.write(cipher.update(buf))
+        outf.write(cipher.final())
+        return outf.getvalue()
+
+##does what it says
+def AESEncrypt(key, iv, text):
+    sbuf=cStringIO.StringIO(text)
+    obuf=cStringIO.StringIO()
+    encoder = EVP.Cipher('aes_128_cfb', key, iv, 1)
+    encrypted = hexlify(cipher_filter(encoder, sbuf, obuf))
+    sbuf.close()
+    obuf.close()
+    return encrypted
+
+##does what it says
+def AESDecrypt(key, iv, text):
+    obuf = cStringIO.StringIO(unhexlify(text))
+    sbuf = cStringIO.StringIO()
+    decoder = EVP.Cipher('aes_128_cfb', key, iv, 0)
+    decrypted = cipher_filter(decoder, obuf, sbuf)
+    sbuf.close()
+    obuf.close()
+    return decrypted
+
 def testCrypto():
     ##saveNewPEM();
     ##print "Saved!"
     ##rsa = loadKeysFromPEM()  ##Hooray works!
-    print getRand32()
-    print getNewAES()
-    print getNewIV()
+    iv = getRand32()
+    iv2 = getRand32()
+    key = getNewAES()
+
+    print "Unencrypted:"
+    print secret
+
+    encrypted = AESEncrypt(key, iv, secret)
+    print encrypted
+    print AESDecrypt(key, iv,encrypted)
 
 if __name__ == "__main__":
     testCrypto()
