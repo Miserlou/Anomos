90375aba6cc74143fbb24447da343b6ba0162a79
Rich Jones <miserlou@gmail.com>
2008-07-23 23:02:46 -0400

    Rate information added to Relayer. All relay = 100% UL

--- Anomos/Relayer.py
+++ Anomos/Relayer.py
@@ -9,37 +9,56 @@ This file is a sort of a mockup and may be rewritten/never used at all.
 
 """
 
-from Anomos import Asizer
-
 class Relayer(object):
+    """ As a tracking code is being sent, each peer it reaches (other than the
+        uploader and downloader) creates a Relayer object to maintain the 
+        association between the incoming socket and the outgoing socket (so 
+        that the TC only needs to be sent once).
+    """
+    def __init__(self, incoming, outgoing, storage, uprate, downrate, choker, key):
+        """
+        @param incoming: The connection to read data from
+        @param outgoing: The connection to write data to
+        @param storage: Where we store data waiting to be sent
+        @param uprate: Upload rate measurer
+        @param downrate: Download rate measurer
 
-    def __init__(self, inconnection, outconnection, choker, neighborpubkey, key):
-        self.asizer = Asizer()
-        self.inconnection = inconnection           """Needed? or handled by Downloader?"""
-        self.outconnection = outconnection      '''Assumed to containa nextneighor IP'''        
+        @type incoming: Connection
+        @type outgoing: Connection
+        @type uprate: Measure
+        @type downrate: Measure
+        @param storage: StorageWrapper
+        """
+        self.incoming = incoming
+        self.outgoing = outgoing
+        self.storage = storage
         self.choker = choker
-        self.relayparts = Queue()                       """Queue for now, while we aren't piecemixing"""
+        self.relayparts = []
         self.choked = True
         self.unchoke_time = None
+        self.uprate = uprate
+        self.downrate = downrate            #statistical
         self.interested = False
         self.buffer = []
-        self.neighborpubkey = neighborpubkey
         self.key = key
 
-    def bytesLessThan(object d, allowedbytes=268435456):
-        size = self.asizer.asizeof(d)
-        if(size < allowedbytes):
-            return true
-        return false
-
-    def addPart(object o):
+    def addPart(self, o):
         ##TODO: Where is this object coming from?
         p = key.decrypt(o)
         relayparts.put(p)
 
-    def returnPart():
+    def returnPart(self):
         return relayparts.get()
 
-     def sendPart():
-       neighborpubkey.encrypt(returnPart())
-       outconnection.write(returnPart())    ## ??
+    def sendPart(self):
+        eprt = self.outgoing.pubkey.encrypt(returnPart())
+        self.outconnection.write(eprt)
+        self.uprate.update_rate(len(eprt))
+
+    def sendOutMessage(message):
+        encm = self.incoming.pubkey.encrypt(message)
+        self.inconnection.write(encm)
+        self.uprate.update_rate(len(encm))
+
+    def get_rate(self):
+        return self.uprate.get_rate()
