879de7268ab8fb70b733ee48bb232da6b829d187
Rich Jones <miserlou@gmail.com>
2009-03-24 23:55:35 -0400

    If windows, send the output to logfile (prevent py2exe exit bug)

--- btdownloadgui.py
+++ btdownloadgui.py
@@ -3037,6 +3037,9 @@ if __name__ == '__main__':
         print str(e)
         sys.exit(1)
 
+    if sys.platform is 'win32':
+        sys.stderr = open("logfile.txt", "w")
+
     advanced_ui = config['advanced']
     ##advanced UI always on!
     ##advanced_ui = 1
