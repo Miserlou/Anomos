6a5dec9eb9555374d82504796b27c10ab296205c
John M. Schanck <jms07@hampshire.edu>
2009-04-23 10:00:50 -0400

    fixed type error in btdownloadheadless

--- btdownloadheadless.py
+++ btdownloadheadless.py
@@ -199,10 +199,10 @@ class HeadlessDisplayer(object):
         print s.getvalue()
 
     def display_relay(self, rate, size, sent):
-        r = "%.3f" % rate
+        r = "%.3f" % float(rate)/1024
         snt = sent
         if snt != self.rsent:
-            print "| relay rate:     " + str((r/1024)) + " KB/s (" + str(size) + ")"
+            print "| relay rate:     " + r + " KB/s (" + str(size) + ")"
             self.rsent = snt
             print '|-'
         else:
