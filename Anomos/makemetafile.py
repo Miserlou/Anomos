c2d4ccaa712329714d07b3872931d26480486fb7
Rich Jones <miserlou@gmail.com>
2009-05-29 18:33:55 -0400

    anon must be a string

--- Anomos/makemetafile.py
+++ Anomos/makemetafile.py
@@ -105,7 +105,7 @@ def make_meta_file(path, url, piece_len_exp, flag=Event(), progress=dummy,
         return
     check_info(info)
     h = file(f, 'wb')
-    data = {'info': info, 'announce': url.strip(), 'creation date': int(bttime()), 'anon': True}
+    data = {'info': info, 'announce': url.strip(), 'creation date': int(bttime()), 'anon': 'True'}
     if comment:
         data['comment'] = comment
     h.write(bencode(data))
