c407225d57ce2a2b37dc412f2b38811ea6674d60
Rich Jones <miserlou@gmail.com>
2009-07-30 16:24:48 -0400

    Remove flickering on button presses

--- btdownloadgui.py
+++ btdownloadgui.py
@@ -355,10 +355,11 @@ class SeedingButton(gtk.Button):
         self.connect('clicked', self.toggle)
         
     def toggle(self, widget):
-        self.paned.remove(self.paned.get_child2())
-        self.paned.pack2(self.mains)
-        self.knowns.hide()
-        self.mains.show()
+        if self.paned.get_child2() is not self.mains:
+            self.paned.remove(self.paned.get_child2())
+            self.paned.pack2(self.mains)
+            self.knowns.hide()
+            self.mains.show()
 
     def send(self, k, m, p):
         self.knowns=k
@@ -379,10 +380,11 @@ class DownloadingButton(gtk.Button):
         self.connect('clicked', self.toggle)
         
     def toggle(self, widget):
-        self.paned.remove(self.paned.get_child2())
-        self.paned.pack2(self.knowns)
-        self.mains.hide()
-        self.knowns.show()
+        if self.paned.get_child2() is not self.knowns:
+            self.paned.remove(self.paned.get_child2())
+            self.paned.pack2(self.knowns)
+            self.mains.hide()
+            self.knowns.show()
 
     def send(self, k, m, p):
         self.knowns=k
