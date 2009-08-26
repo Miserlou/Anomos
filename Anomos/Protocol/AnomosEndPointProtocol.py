499b37193eb7e46ffd2e69a175b3453538c26bc1
John M. Schanck <jms07@hampshire.edu>
2009-08-26 09:56:49 -0400

    That endpoint test case was bullshit

--- Anomos/Protocol/AnomosEndPointProtocol.py
+++ Anomos/Protocol/AnomosEndPointProtocol.py
@@ -51,13 +51,12 @@ class AnomosEndPointProtocol(AnomosProtocol):
     def got_relay(self, message):
         self.got_message(message[1:])
     def got_encrypted(self, message):
-        if self.complete:
-            raise RuntimeError("Received encrypted data \
-                                before connection was complete")
         if self.e2e_key is not None:
             m = self.e2e_key.decrypt(message[1:])
             self.got_message(m)
         else:
+            import pdb
+            pdb.set_trace()
             raise RuntimeError("Received encrypted data before we were ready")
     def got_break(self):
         self.recvd_break = True
