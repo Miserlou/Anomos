b7299bb8e288b7c54ccf87e425bd062e2c7af9a8
John M. Schanck <jms07@hampshire.edu>
2009-09-20 15:34:52 -0400

    stupid hack

--- Anomos/Relayer.py
+++ Anomos/Relayer.py
@@ -102,7 +102,7 @@ class Relayer(AnomosRelayerProtocol):
         if self.closed:
             self.logfunc(WARNING, "Double close")
             return
-        if not (self.decremented_count or self.orelay.decremented_count):
+        if not (self.decremented_count or (self.orelay and self.orelay.decremented_count)):
             self.manager.dec_relay_count()
             self.decremented_count = True
         self.closed = True
