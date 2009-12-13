0d0b29f1d9f210e818dc1bc3c4f2ec1b48b9156c
Rich <tuttle@ketnap.(none)>
2009-12-13 16:34:43 -0500

    Functionify the magnet parser

--- Anomos/magnet.py
+++ Anomos/magnet.py
@@ -1,30 +1,27 @@
 import urlparse
 
-magnet = "magnet:?xt=urn:btih:afa23e62c4ca6965800815329553c6a705cdb2f4&dn=%5BPSP%5DHello.Kitty.Happy.Accessory.%5BJAP%5D.-..rar"
-invalid = 'Invalid'
+def parse_magnet(magnet):
+    if magnet[:8] != "magnet:?":
+        raise NameError(invalid)
+    else:
+        magnet = magnet[8:]
+    args =  urlparse.parse_qs(magnet)
 
-if magnet[:8] != "magnet:?":
-    print invalid
-else:
-    magnet = magnet[8:]
-args =  urlparse.parse_qs(magnet)
-print args
+    btih = "xt=urn:btih:"
+    # magnet is retardedly designed so this hack is needed to get the has
+    if magnet.find(btih) == -1:
+        print invalid
+    else:
+        bindex = magnet.find(btih)
+        qindex = magnet.find('&')
+        hsh = magnet[bindex+12:qindex]
+        print hsh
 
-btih = "xt=urn:btih:"
-# magnet is retardedly designed so this hack is needed to get the has
-if magnet.find(btih) == -1:
-    print invalid
-else:
-    bindex = magnet.find(btih)
-    qindex = magnet.find('&')
-    hsh = magnet[bindex+12:qindex]
-    print hsh
+    if not 'dn' in args:
+        dn = "Missing name"
+    else:
+        dn = str(args['dn'])
+        dn = dn[2:]
+        dn = dn[:-2]
 
-if not 'dn' in args:
-    dn = "Missing name"
-else:
-    dn = str(args['dn'])
-    dn = dn[2:]
-    dn = dn[:-2]
-
-print hsh, dn
+    return [hsh, dn]
