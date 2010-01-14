12dcd499f6fa0d14cafcf3fb98fef32d47f22299
John M. Schanck <jms07@hampshire.edu>
2010-01-14 03:28:47 -0500

    Removed unused attributes from PeerCert

--- Anomos/Crypto/_PeerCert.py
+++ Anomos/Crypto/_PeerCert.py
@@ -1,3 +1,16 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import hashlib
 
 from M2Crypto import RSA, X509
@@ -8,12 +21,9 @@ import Anomos.Crypto
 
 class PeerCert:
     def __init__(self, certObj):
-        self.certificate = certObj
         self.hash_alg = 'sha256'
-        self.fingerprint = self.certificate.get_fingerprint(self.hash_alg)
-        tmp = X509.load_cert_string(certObj.as_pem()).get_pubkey().get_rsa()
-        self.pubkey = RSA.new_pub_key((tmp.e, tmp.n))
-        self.randfile = global_randfile
+        self.fingerprint = certObj.get_fingerprint(self.hash_alg)
+        self.pubkey = X509.load_cert_string(certObj.as_pem()).get_pubkey().get_rsa()
 
     def cmp(self, certObj):
         return self.fingerprint == certObj.get_fingerprint(self.hash_alg)
