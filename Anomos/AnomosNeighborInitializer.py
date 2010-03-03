de15e209568cf18e7caa83d5b7cb8b6c815b054b
Rich Jones <rich@anomos.info>
2010-03-03 15:18:12 -0500

    Avoid logging error

--- Anomos/AnomosNeighborInitializer.py
+++ Anomos/AnomosNeighborInitializer.py
@@ -81,8 +81,10 @@ class AnomosNeighborInitializer(object):
                        self.protocol_extensions()
         self.socket.push(hdr)
     def connection_closed(self):
-        if self.id != NAT_CHECK_ID:
+        if self.id != NAT_CHECK_ID and self.id != '':
             log.info("Failed to initialize connection to %s" % str(self.id))
+        else:
+            log.info("Failed to initialize a connection")
         if not self.complete:
             self.manager.initializer_failed(self.id)
         self.socket = None
